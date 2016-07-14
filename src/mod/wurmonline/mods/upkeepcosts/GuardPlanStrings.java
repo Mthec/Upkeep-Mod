51dc2717f904007ace8a667630c56cf9f17899f4
Daniel <daniel4592@hotmail.com>
2016-07-14 23:28:12 +0100

    Removed unnecessary spaces.

--- src/mod/wurmonline/mods/upkeepcosts/GuardPlanStrings.java
+++ src/mod/wurmonline/mods/upkeepcosts/GuardPlanStrings.java
@@ -73,66 +73,60 @@ public class GuardPlanStrings {
             "if (this.output) {" +
             "    System.out.println(\"Village upkeep - \" + this.getVillage().getName() + \" paid \" + Double.toString(upkeepD) + \" this turn.  Upkeep buffer is now \" + Double.toString(this.upkeepBuffer));" +
             "}" +
-            // TODO - Spaces.
-            "        long upkeep = (long)upkeepD;" +
-            "        if(this.moneyLeft - upkeep <= 0L) {" +
-            "            try {" +
-            "                logger.log(java.util.logging.Level.INFO, this.getVillage().getName() + \" disbanding. Money left=\" + this.moneyLeft + \", upkeep=\" + upkeep);" +
-            "            } catch (com.wurmonline.server.villages.NoSuchVillageException var6) {" +
-            "                logger.log(java.util.logging.Level.INFO, var6.getMessage(), var6);" +
-            "            }" +
-            "" +
-            "            return true;" +
+            "long upkeep = (long)upkeepD;" +
+            "if(this.moneyLeft - upkeep <= 0L) {" +
+            "    try {" +
+            "        logger.log(java.util.logging.Level.INFO, this.getVillage().getName() + \" disbanding. Money left=\" + this.moneyLeft + \", upkeep=\" + upkeep);" +
+            "    } catch (com.wurmonline.server.villages.NoSuchVillageException var6) {" +
+            "        logger.log(java.util.logging.Level.INFO, var6.getMessage(), var6);" +
+            "    }" +
+            "    return true;" +
+            "} else {" +
+            "    if(upkeep >= 100L) {" +
+            "        try {" +
+            "            logger.log(java.util.logging.Level.INFO, this.getVillage().getName() + \" upkeep=\" + upkeep);" +
+            "        } catch (com.wurmonline.server.villages.NoSuchVillageException var10) {" +
+            "            logger.log(java.util.logging.Level.INFO, var10.getMessage(), var10);" +
+            "        }" +
+            "    }" +
+            "    this.updateGuardPlan(this.type, this.moneyLeft - upkeep, this.hiredGuardNumber);" +
+            "    ++this.upkeepCounter;" +
+            "    if(this.upkeepCounter == 2) {" +
+            "        this.upkeepCounter = 0;" +
+            "        com.wurmonline.server.economy.Shop tl = com.wurmonline.server.economy.Economy.getEconomy().getKingsShop();" +
+            "        if(tl != null) {" +
+            "            tl.setMoney(tl.getMoney() + upkeep);" +
             "        } else {" +
-            "            if(upkeep >= 100L) {" +
-            "                try {" +
-            "                    logger.log(java.util.logging.Level.INFO, this.getVillage().getName() + \" upkeep=\" + upkeep);" +
-            "                } catch (com.wurmonline.server.villages.NoSuchVillageException var10) {" +
-            "                    logger.log(java.util.logging.Level.INFO, var10.getMessage(), var10);" +
-            "                }" +
+            "           logger.log(java.util.logging.Level.WARNING, \"No shop when \" + this.villageId + \" paying upkeep.\");" +
+            "        }" +
+            "    }" +
+            "    long var12 = this.getTimeLeft();" +
+            "    if(var12 < 3600000L) {" +
+            "        try {" +
+            "            this.getVillage().broadCastAlert(\"The village is disbanding within the hour. You may add upkeep money to the village coffers at the token immediately.\", (byte)2);" +
+            "            this.getVillage().broadCastAlert(\"Any traders who are citizens of \" + this.getVillage().getName() + \" will disband without refund.\");" +
+            "        } catch (com.wurmonline.server.villages.NoSuchVillageException var9) {" +
+            "            logger.log(java.util.logging.Level.WARNING, \"No Village? \" + this.villageId, var9);" +
+            "        }" +
+            "    } else if(var12 < 86400000L) {" +
+            "        if(System.currentTimeMillis() - this.lastSentWarning > 3600000L) {" +
+            "            this.lastSentWarning = System.currentTimeMillis();" +
+            "            try {" +
+            "                this.getVillage().broadCastAlert(\"The village is disbanding within 24 hours. You may add upkeep money to the village coffers at the token.\", (byte)2);" +
+            "                this.getVillage().broadCastAlert(\"Any traders who are citizens of \" + this.getVillage().getName() + \" will disband without refund.\");" +
+            "            } catch (com.wurmonline.server.villages.NoSuchVillageException var8) {" +
+            "                logger.log(java.util.logging.Level.WARNING, \"No Village? \" + this.villageId, var8);" +
             "            }" +
-            "" +
-            "            this.updateGuardPlan(this.type, this.moneyLeft - upkeep, this.hiredGuardNumber);" +
-            "            ++this.upkeepCounter;" +
-            "            if(this.upkeepCounter == 2) {" +
-            "                this.upkeepCounter = 0;" +
-            "                com.wurmonline.server.economy.Shop tl = com.wurmonline.server.economy.Economy.getEconomy().getKingsShop();" +
-            "                if(tl != null) {" +
-            "                    tl.setMoney(tl.getMoney() + upkeep);" +
-            "                } else {" +
-            "                    logger.log(java.util.logging.Level.WARNING, \"No shop when \" + this.villageId + \" paying upkeep.\");" +
-            "                }" +
-            "            }" +
-            "" +
-            "            long var12 = this.getTimeLeft();" +
-            "            if(var12 < 3600000L) {" +
-            "                try {" +
-            "                    this.getVillage().broadCastAlert(\"The village is disbanding within the hour. You may add upkeep money to the village coffers at the token immediately.\", (byte)2);" +
-            "                    this.getVillage().broadCastAlert(\"Any traders who are citizens of \" + this.getVillage().getName() + \" will disband without refund.\");" +
-            "                } catch (com.wurmonline.server.villages.NoSuchVillageException var9) {" +
-            "                    logger.log(java.util.logging.Level.WARNING, \"No Village? \" + this.villageId, var9);" +
-            "                }" +
-            "            } else if(var12 < 86400000L) {" +
-            "                if(System.currentTimeMillis() - this.lastSentWarning > 3600000L) {" +
-            "                    this.lastSentWarning = System.currentTimeMillis();" +
-            "" +
-            "                    try {" +
-            "                        this.getVillage().broadCastAlert(\"The village is disbanding within 24 hours. You may add upkeep money to the village coffers at the token.\", (byte)2);" +
-            "                        this.getVillage().broadCastAlert(\"Any traders who are citizens of \" + this.getVillage().getName() + \" will disband without refund.\");" +
-            "                    } catch (com.wurmonline.server.villages.NoSuchVillageException var8) {" +
-            "                        logger.log(java.util.logging.Level.WARNING, \"No Village? \" + this.villageId, var8);" +
-            "                    }" +
-            "                }" +
-            "            } else if(var12 < 604800000L && System.currentTimeMillis() - this.lastSentWarning > 3600000L) {" +
-            "                this.lastSentWarning = System.currentTimeMillis();" +
-            "" +
-            "                try {" +
-            "                    this.getVillage().broadCastAlert(\"The village is disbanding within one week. Due to the low morale this gives, the guards have ceased their general maintenance of structures.\", (byte)4);" +
-            "                    this.getVillage().broadCastAlert(\"Any traders who are citizens of \" + this.getVillage().getName() + \" will disband without refund.\");" +
-            "                } catch (com.wurmonline.server.villages.NoSuchVillageException var7) {" +
-            "                    logger.log(java.util.logging.Level.WARNING, \"No Village? \" + this.villageId, var7);" +
-            "                }" +
-            "            }" +
-            "            return false;" +
-            "        }}";
+            "        }" +
+            "   } else if(var12 < 604800000L && System.currentTimeMillis() - this.lastSentWarning > 3600000L) {" +
+            "        this.lastSentWarning = System.currentTimeMillis();" +
+            "        try {" +
+            "            this.getVillage().broadCastAlert(\"The village is disbanding within one week. Due to the low morale this gives, the guards have ceased their general maintenance of structures.\", (byte)4);" +
+            "            this.getVillage().broadCastAlert(\"Any traders who are citizens of \" + this.getVillage().getName() + \" will disband without refund.\");" +
+            "        } catch (com.wurmonline.server.villages.NoSuchVillageException var7) {" +
+            "            logger.log(java.util.logging.Level.WARNING, \"No Village? \" + this.villageId, var7);" +
+            "        }" +
+            "    }" +
+            "    return false;" +
+            "}}";
 }
